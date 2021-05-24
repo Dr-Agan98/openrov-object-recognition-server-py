5f99a5e2156236ab4f7f3c5ea87a43a37a74332f
Dragan Vuletic <dragan.vule98@gmail.com>
2021-05-24 10:05:34 +0200

    Added response message with bd_box

--- server.py
+++ server.py
@@ -1,21 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
-from flask_socketio import SocketIO
-from flask import request
+from flask_socketio import SocketIO,emit
 from PIL import Image
 import base64, io
 import numpy as np
 from yolov4 import Detector
+import json
 
 app = Flask(__name__)
 CORS(app)
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
 
 d = Detector(config_path="/home/nagard/Progetti/openrov-object-recognition-server-py/yolov4-tiny-3l.cfg", weights_path="/home/nagard/Progetti/openrov-object-recognition-server-py/yolov4-tiny-3l_best_2.weights" , meta_path="/home/nagard/Progetti/openrov-object-recognition-server-py/data/detector.data")
 
-#@app.route("/",methods=['GET','POST'])
-#def root():
-#    return '{box:null}'
+@app.route("/",methods=['GET','POST'])
+def root():
+    return '{box:null}'
 
 @socketio.on("test")
 def handle_my_custom_event(data):
@@ -23,8 +23,17 @@ def handle_my_custom_event(data):
     img_arr = np.array(img.resize((d.network_width(), d.network_height())))
     detections = d.perform_detect(image_path_or_buf=img_arr, show_image=False)
     for detection in detections:
-        box = detection.left_x, detection.top_y, detection.width, detection.height
-        print(f'{detection.class_name.ljust(10)} | {detection.class_confidence * 100:.1f} % | {box}')
-    
+        box = {
+            'class': detection.class_name.ljust(10),
+            'confidence': detection.class_confidence,
+            'box':{
+                'x': detection.left_x,
+                'y': detection.top_y,
+                'w': detection.width,
+                'h': detection.height
+            }       
+        }
+        emit("detection", json.dumps(box))
+        #print(f'{detection.class_name.ljust(10)} | {detection.class_confidence * 100:.1f} % | {box}')
 
 socketio.run(app, host="0.0.0.0")
